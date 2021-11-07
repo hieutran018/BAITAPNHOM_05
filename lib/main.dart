26266fa383be6bd3078beb76a900cb6a8b3435f3
anh-huynh-1810 <0306191002@caothang.du.vn>
2021-11-07 22:57:50 +0700

    Vân Anh update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +7,178 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  // String email;
+  // String password;
+  // void Covert() {
+  //   setState(() {});
+  //   email = _email.text;
+  //   password = _password.text;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.blue[400],
+        body: Column(
+          children: [
+            SizedBox(height: 100),
+            Container(
+              width: 150,
+              height: 150,
+              child: Image.asset('./images/imageLogin.png'),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "Signln",
+              textAlign: TextAlign.center,
+              textScaleFactor: 5,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "Speak, friend, and enter",
+              textAlign: TextAlign.center,
+              textScaleFactor: 2,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: TextField(
+                controller: _email,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Email'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: TextField(
+                controller: _password,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Password'),
+              ),
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextButton(
+                onPressed: () {},
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Colors.black,
+                        // padding: EdgeInsets.all(55),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 150, vertical: 30)),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const LoginUnsuccessfulPage()),
+                      );
+                    },
+                    child: Text(
+                      "SIGN IN",
+                    )),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
+  }
+}
+
+class LoginUnsuccessfulPage extends StatelessWidget {
+  const LoginUnsuccessfulPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            SizedBox(height: 100),
+            Container(
+              width: 150,
+              height: 150,
+              child: Image.asset('./images/imageLoginUnsuccessful.png'),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+            ),
+            Text(
+              "UPS... couldn't Sign in",
+              textAlign: TextAlign.center,
+              textScaleFactor: 2,
+              style: TextStyle(
+                fontSize: 20.0,
+                //  color: Colors.black,
+              ),
+            ),
+            Text(
+              "Your email and password don't match.\n Please, try again. ",
+              textAlign: TextAlign.center,
+              textScaleFactor: 1,
+              style: TextStyle(
+                  //color: Colors.black,
+                  ),
+            ),
+            SizedBox(height: 500),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextButton(
+                onPressed: () {},
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Colors.black,
+                        // padding: EdgeInsets.all(55),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 200, vertical: 30)),
+                    onPressed: () {
+                      Navigator.pop(
+                        context,
+                      );
+                    },
+                    child: Text(
+                      "TRY AGAIN",
+                    )),
+              ),
+            ),
+          ],
+        ));
   }
 }
